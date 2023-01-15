6ddca8a939e4ff01d24b4d6c8733332277c10da1
oddgrd <29732646+oddgrd@users.noreply.github.com>
2023-01-15 21:23:27 +0100

    fish: update git alias

--- config.fish
+++ config.fish
@@ -35,20 +35,12 @@ alias ga='git add'
 
 #
 # Will return the current branch name
-# Usage example: git pull origin $(current_branch)
+# Usage example: git pull origin $(curr)
 #
-function current_branch
-  set ref (git symbolic-ref HEAD 2> /dev/null); or \
-  set ref (git rev-parse --short HEAD 2> /dev/null); or return
-  echo ref | sed s-refs/heads--
-end
-
-function current_repository
-  set ref (git symbolic-ref HEAD 2> /dev/null); or \
-  set ref (git rev-parse --short HEAD 2> /dev/null); or return
-  echo (git remote -v | cut -d':' -f 2)
+function curr
+  git branch --show-current  
 end
 
 # these aliases take advantage of the previous function
-alias ggpull='git pull origin (current_branch)'
-alias ggpush='git push origin (current_branch)'
+alias ggpull='git pull origin (curr)'
+alias ggpush='git push origin (curr)'
